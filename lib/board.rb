f4099de0c8eb9787bd55a8f2ef1e6ad570c3f769
Ali Yousif <alis.yousif@gmail.com>
2020-01-31 14:58:55 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,14 +1,2 @@
 # Define the variable board below.
-row_1 = [" "," "," "]
-# Position 1: left
-row[0]
-# Position 2: Middle
-row[1]
-# position 3: right
-row[2]
-
-row[1] = X
-
-row_2 = [" "," "," "]
-
-row_3 = [" "," "," "]
+board = [" "," "," "," "," "," "," "," "," "]
